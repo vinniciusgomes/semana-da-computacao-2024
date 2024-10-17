fcbafd0c174a5f3c704d91c0b5da5fe99318b520
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-10-17 09:55:38 -0300

    feat: aula 01

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,9 @@
-"use client";
+import { styled } from "styled-components";
 
-import { ReactNode } from "react";
+const StyledButton = styled.button`
+  background-color: red;
+`;
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
+export const Button = () => {
+  return <StyledButton>Test</StyledButton>;
 };
